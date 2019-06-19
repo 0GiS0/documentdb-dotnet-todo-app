7698f2a8e4c982935f53bf96be52c4f0ffefc52f
Gisela Torres Buitrago <giselatb@outlook.com>
2019-06-19 17:25:50 +0000

    echo %documentEndpoint%

--- create-cosmosdb-for-tests.bat
+++ create-cosmosdb-for-tests.bat
@@ -13,8 +13,4 @@ CALL az group create --name %RESOURCE_GROUP% --location %LOCATION%
 
 CALL az cosmosdb create --resource-group %RESOURCE_GROUP% --name %ACCOUNT_NAME% --kind GlobalDocumentDB
 
-SET documentEndpoint = $(az cosmosdb show --name %ACCOUNT_NAME% --resource-group %RESOURCE_GROUP% --query "documentEndpoint")
-
-echo "Document endpoint:"
-echo $documentEndpoint
 echo "##vso[task.setvariable variable=CosmosDBEndpoint]%ACCOUNT_NAME%.documents.azure.com:443/"
