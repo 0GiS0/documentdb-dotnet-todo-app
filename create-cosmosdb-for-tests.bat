bfd04d2621775df60e14211294a1fdfc3d0ec051
Gisela Torres Buitrago <giselatb@outlook.com>
2019-06-19 17:51:00 +0000

    Added CosmosDB Key

--- create-cosmosdb-for-tests.bat
+++ create-cosmosdb-for-tests.bat
@@ -13,4 +13,10 @@ CALL az group create --name %RESOURCE_GROUP% --location %LOCATION%
 
 CALL az cosmosdb create --resource-group %RESOURCE_GROUP% --name %ACCOUNT_NAME% --kind GlobalDocumentDB
 
+CALL az cosmosdb list-keys --name %ACCOUNT_NAME% --resource-group %RESOURCE_GROUP% --query "primaryMasterKey" --output tsv>primaryKey
+set /p key= < primaryKey 
+
+
+
 echo ##vso[task.setvariable variable=CosmosDBEndpoint]https://%ACCOUNT_NAME%.documents.azure.com:443/
+echo ##vso[task.setvariable variable=CosmosDBAuthKey]%key%
